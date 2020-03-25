7c0fb97f8d71d5b17f3751f06d8b229fc46cc9a5
Ofeliia Bagratian <58888440+ofeliiaB@users.noreply.github.com>
2020-03-25 12:35:49 +0100

    Update Theatre_Community_Database.sql

--- Theatre_Community_Database.sql
+++ Theatre_Community_Database.sql
@@ -1,9 +1,4 @@
-﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/TEhQiT
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- To reset the sample schema, replace everything with
--- two dots ('..' - without quotes).
 
 CREATE TABLE `customer` (
     `id` int  NOT NULL ,
